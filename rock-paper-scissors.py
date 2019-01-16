b022ba26b408c17f56c3d37153cee9297ad79c34
Nipun Tyagi <sumittyagi20@gmail.com>
2019-01-16 08:33:40 +0530

    Create rock-paper-scissors.py

---
+++ rock-paper-scissors.py
@@ -0,0 +1,29 @@
+# Rock beats scissors   and  paper beats rock
+from random import randint
+player = input('rock(r), paper(p) or scissors(s)?')
+chosen=randint(1,3)
+if(chosen == 1):
+  chosen='r'
+elif(chosen == 2):
+  chosen='p'
+elif(chosen == 3):
+  chosen='s' 
+print(player,' Vs ',chosen)
+if(player == chosen):
+    print('Draw!!',n)
+    n=n+1
+elif(player == 'p' and chosen=='r'):
+    print('Gotcha You Win!!')
+elif(player == 'p' and chosen=='s'):
+    print('Gotcha You Win!!')
+elif(player == 'r' and chosen=='s'):
+    print('Gotcha You Win!!')
+elif(player == 'r' and chosen=='p'):
+    print('Oohh You Lose!!',m)
+    m=m+1
+elif(player == 's' and chosen=='r'):
+    print('Oohh You Lose!!',m)
+    m=m+1
+elif(player == 's' and chosen=='p'):
+    print('Oohh You Lose!!',m)
+    m=m+1
